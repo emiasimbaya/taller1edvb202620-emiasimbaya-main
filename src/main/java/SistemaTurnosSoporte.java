2ffbd4e4e23accec3923ed3033d924932e2a4640
emiasimbaya <e_asimbaya@hotmail.com>
2026-04-14 14:17:31 -0500

    2n commit

---
+++ src/main/java/SistemaTurnosSoporte.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SistemaTurnosSoporte {
+
+    private Queue<String> cola;
+    private static final int CAPACIDAD_MAXIMA = 10;
+
+    public SistemaTurnosSoporte(String turno, int numeroDeTurno) {
+
+
+    }
+
+    public boolean registrarTurno(String codigo, String problema) {
+
+    }
+
+    public String verSiguienteTurno(int numeroDeTurno) {
+    numeroDeTurno
+    }
+
+    public String atenderSiguienteTurno() {
+
+    }
+
+    public int obtenerCantidadTurnos() {
+
+    }
+
+    public int obtenerEspaciosDisponibles() {
+
+    }
+
+    public String mostrarCola() {
+
+    }
+}
